345c2b66d4cec370217dd2328854d94d55f07fa7
Isabe Alvarado Blanco Uribe <alvaradoblancouribe@gmail.com>
2017-07-27 11:15:17 -0400

    views.py

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
 
-urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
-]
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
